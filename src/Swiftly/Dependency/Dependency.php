4aeaeb95da4dac65508060b1e7f3f048c2e530b1
clvarley <conorlukevarley@gmail.com>
2021-01-08 17:17:43 +0000

    Type infering utility

--- src/Swiftly/Dependency/Dependency.php
+++ src/Swiftly/Dependency/Dependency.php
@@ -3,6 +3,7 @@
 namespace Swiftly\Dependency;
 
 use Swiftly\Dependency\Container;
+use Closure;
 
 /**
  * Wraps a dependency in the dependency container
@@ -11,6 +12,48 @@ use Swiftly\Dependency\Container;
  */
 Class Dependency
 {
+
+    /**
+     * Indicates the callback is invalid
+     *
+     * @var int TYPE_INVALID Invalid callback
+     */
+    const TYPE_INVALID = 0;
+
+    /**
+     * Indicates the callback is a standard function
+     *
+     * @var int TYPE_FUNCTION Standard function
+     */
+    const TYPE_FUNCTION = 1;
+
+    /**
+     * Indicates the callback is a class method
+     *
+     * @var int TYPE_METHOD Class method
+     */
+    const TYPE_METHOD = 2;
+
+    /**
+     * Indicates the callback is a static function
+     *
+     * @var int TYPE_STATIC Static function
+     */
+    const TYPE_STATIC = 3;
+
+    /**
+     * Indicates the callback is an invokable object
+     *
+     * @var int TYPE_INVOKABLE Invokable object
+     */
+    const TYPE_INVOKABLE = 4;
+
+    /**
+     * Indicates the callback is a closure
+     *
+     * @var int TYPE_CLOSURE Closure function
+     */
+    const TYPE_CLOSURE = 5;
 
     /**
      * The parent container for this dependency
@@ -80,6 +123,7 @@ Class Dependency
     {
         $result = null;
 
+        $type =
         if ( \is_callable( $this->implementation ) ) {
             $callback = $this->implementation;
             $result = $callback( $this->container );
@@ -95,6 +139,57 @@ Class Dependency
         }
 
         return $result;
+    }
+
+    /**
+     * Attempts to infer the type of given callable
+     *
+     * @param mixed $callable Callable variable
+     * @return int            TYPE_* constant
+     */
+    public static function inferType( $callable ) : int
+    {
+        // Nice and easy!
+        if ( $callable instanceof Closure ) {
+            return self::TYPE_CLOSURE;
+        }
+
+        // Function name or possible static call?
+        if ( \is_string( $callable ) ) {
+            if ( \strpos( $callable, '::' ) === false ) {
+                return self::TYPE_FUNCTION;
+            }
+
+            $callable = \explode( '::', $callable );
+        }
+
+        // Neither a static or method call?
+        if ( !\is_array( $callable ) || \count( $callable ) !== 2 ) {
+            return self::TYPE_INVALID;
+        }
+
+        list( $class, $method ) = $callable;
+
+        // Has to be a string by this point
+        if ( !\is_string( $method ) ) {
+            return self::TYPE_INVALID;
+        }
+
+        if ( \is_object( $class ) ) {
+            return self::TYPE_METHOD;
+        }
+
+        // Static method or just uninitialised object?
+        try {
+            $reflected = new \ReflectionMethod( $class, $name );
+        } catch ( \ReflectionException $e ) {
+            return self::TYPE_INVALID;
+        }
+
+        return ( $reflected->isStatic()
+            ? self::TYPE_STATIC
+            : self::TYPE_METHOD
+        );
     }
 
     /**
