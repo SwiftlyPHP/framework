ab5f8ea1c142dbad3e3928a80d27d2e519dd05f8
clvarley <conorlukevarley@gmail.com>
2021-01-25 23:01:45 +0000

    Autoloader doesn't need namespace escaping!

--- src/Swiftly/utilities/Autoloader.php
+++ src/Swiftly/utilities/Autoloader.php
@@ -18,7 +18,7 @@ Final Class Autoloader
      */
     public function __construct()
     {
-        \spl_autoload_register([ $this, 'find' ]);
+        spl_autoload_register([ $this, 'find' ]);
     }
 
     /**
@@ -29,8 +29,8 @@ Final Class Autoloader
      */
     public function addPrefix( string $prefix, string $path ) : void
     {
-        if ( \is_dir( $path ) ) {
-            $this->prefixes[\strtolower( $prefix )] = \rtrim( $path, '/' ) . '/';
+        if ( is_dir( $path ) ) {
+            $this->prefixes[strtolower( $prefix )] = rtrim( $path, '/' ) . '/';
         }
     }
 
@@ -45,13 +45,13 @@ Final Class Autoloader
     public function find( string $class_name ) : void
     {
 
-        $parts = \explode( '\\', $class_name );
-        $class = \array_pop( $parts );
-        $vendor = \array_shift( $parts );
+        $parts = explode( '\\', $class_name );
+        $class = array_pop( $parts );
+        $vendor = array_shift( $parts );
 
         // Get class vendor
         if ( !empty( $vendor ) ) {
-            $vendor = \strtolower( $vendor );
+            $vendor = strtolower( $vendor );
         } else {
             $vendor = '*';
         }
@@ -60,23 +60,23 @@ Final Class Autoloader
         if ( !empty( $this->prefixes[$vendor] ) ) {
             $dir_path = $this->prefixes[ $vendor ];
         } else {
-            $dir_path = \get_include_path() . '/';
+            $dir_path = get_include_path() . '/';
         }
 
         // Attempt 1. Simple file include?
-        if ( \is_file( $file = $dir_path . \implode( '/', $parts ) . "/$class.php" ) ) {
+        if ( is_file( $file = $dir_path . implode( '/', $parts ) . "/$class.php" ) ) {
             require $file;
             return;
         }
 
         // Atempt 2. Traverse directories
         while ( !empty( $parts ) ) {
-            $directory = \scandir( $dir_path, \SCANDIR_SORT_NONE );
+            $directory = scandir( $dir_path, SCANDIR_SORT_NONE );
 
-            $current = \array_shift( $parts );
+            $current = array_shift( $parts );
 
             foreach ( $directory as $dir ) {
-                if ( \strcasecmp( $dir, $current ) === 0 ) {
+                if ( strcasecmp( $dir, $current ) === 0 ) {
                     $dir_path .= "$dir/";
                     continue 2;
                 }
@@ -85,16 +85,16 @@ Final Class Autoloader
             return;
         }
 
-        $files = \scandir( $dir_path, \SCANDIR_SORT_NONE );
-        $class = \strtolower( $class );
+        $files = scandir( $dir_path, SCANDIR_SORT_NONE );
+        $class = strtolower( $class );
 
         foreach ( $files as $file ) {
-            if ( \strtolower( \substr( $file, 0, -4 ) ) === $class ) {
+            if ( strtolower( substr( $file, 0, -4 ) ) === $class ) {
                 require $dir_path . $file;
                 return;
             }
         }
 
-        return; // Not found! :(
+        return; // Not found!
     }
 }
