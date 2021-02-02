a049118ca2f8419917d0411cc9bb24554f533da9
clvarley <conorlukevarley@gmail.com>
2021-02-02 21:08:29 +0000

    Created CacheWriter middleware

---
+++ src/Swiftly/Middleware/CacheWriterMiddleware.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Swiftly\Middleware;
+
+use Swiftly\Http\Server\{
+    Request,
+    Response
+};
+use Swiftly\Middleware\MiddlewareInterface;
+
+/**
+ * Middleware responsible for caching HTML responses
+ *
+ * @author clvarley
+ */
+Class CacheWriterMiddleware Implements MiddlewareInterface
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run( Request $request, Response $response, callable $next ) : Response
+    {
+        // Only cache successful (HTTP 200) GET requests with NO parameters
+        if ( $request->getMethod() !== 'GET'
+            || $response->getStatus() !== 200
+            || $request->query->all()
+        ) {
+            return $next( $request, $response );
+        }
+
+        // Is "Cache-Control: no-store" set?
+        $cache_control = $request->headers->get( 'Cache-Control' );
+        $cache_control = $cache_control ? \strtolower( $cache_control ) : '';
+
+        if ( \strpos( $cache_control, 'no-store' ) !== false ) {
+            return $next( $request, $response );
+        }
+
+        $hash = \sha1( $request->getPath() );
+        $dir = APP_DATA . 'cache/html';
+        $file = "$dir/$hash.php";
+
+        // Already cached!
+        if ( \is_file( $file ) {
+            return $next( $request, $response );
+        }
+
+        // Failed to make directory
+        if ( !\is_dir( $dir ) && !\mkdir( $dir, 0644 ) ) {
+            return $next( $request, $response );
+        }
+
+        // Cache the content!
+        $content = $response->getContent();
+        file_put_contents( $file, $content,  );
+
+        return $next( $request, $response );
+    }
+}
