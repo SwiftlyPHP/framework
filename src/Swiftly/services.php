8e8de6a04643a71adb7eba5d65d355c50958d3d4
clvarley <conorlukevarley@gmail.com>
2022-04-22 18:56:30 +0100

    Temp fix for the routing init

--- src/Swiftly/services.php
+++ src/Swiftly/services.php
@@ -39,7 +39,12 @@ return [
     ],
 
     // Route parser
-    Swiftly\Routing\Collection\RouteCollection::class => Swiftly\Routing\Collection\RouteCollection::class,
+    Swiftly\Routing\Collection::class => [
+        'handler' => function ( Swiftly\Routing\ProviderInterface $provider ) {
+            return $provider->populate( new Swiftly\Routing\Collection() );
+        }
+    ],
     Swiftly\Routing\ProviderInterface::class => Swiftly\Routing\Provider\JsonProvider::class,
+    Swiftly\Routing\CompilerInterface::class => Swiftly\Routing\Compiler\StandardCompiler::class,
     Swiftly\Routing\Dispatcher::class => Swiftly\Routing\Dispatcher::class
 ];
