b5b69910cd5928c866f974326cfac9b84d2a84f2
clvarley <conorlukevarley@gmail.com>
2023-08-16 15:10:26 +0100

    Use correct ref to $posts

--- app/controller/Example5.php
+++ app/controller/Example5.php
@@ -3,15 +3,15 @@
 namespace App\Controller;
 
 use Swiftly\Base\AbstractController;
-use Swiftly\Http\Server\Response;
 use App\Model\PostProvider;
+use Swiftly\Http\Server\Response;
 
 use function count;
 
 /**
  * Class used to represent an archive of posts
  * 
- * @example Returning non-HTML responses
+ * @example URL generation, returning non-HTML responses
  */
 class Example5 extends AbstractController
 {
@@ -26,7 +26,7 @@ class Example5 extends AbstractController
         return $this->output('archive.html.php', [
             'title' => 'Search results',
             'count' => count($posts),
-            'posts' => $provider->getPosts(['page' => $page])
+            'posts' => $posts
         ]);
     }
 }
